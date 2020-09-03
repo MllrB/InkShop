84cb22258df592343235341b6c53d1b94adc9195
Brian Miller <brian@theinkshop.ie>
2020-09-03 20:54:40 +0100

    Basic unpacking of product searches and styling begun

--- products/views.py
+++ products/views.py
@@ -11,20 +11,22 @@ def all_products(request):
     A view to return all products and filter results for user searches
     """
     products = Supplies.objects.all()
+    user_search = None
 
     if request.method == 'GET':
-        query_terms = request.GET['q']
-        if not query_terms:
+        user_search = request.GET['q']
+        if not user_search:
             messages.error(request, 'What exactly are you looking for?')
             return redirect(reverse('home'))
 
-        queries = Q(skus__icontains=query_terms) | Q(
-            name__icontains=query_terms) | Q(keywords__icontains=query_terms) | Q(title__contains=query_terms)
+        queries = Q(skus__icontains=user_search) | Q(
+            name__icontains=user_search) | Q(keywords__icontains=user_search) | Q(title__contains=user_search)
 
         products = products.filter(queries)
 
     context = {
         'products': products,
+        'search': user_search,
     }
 
     return render(request, 'products/products.html', context)
