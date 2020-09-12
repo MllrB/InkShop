5671992bb884201c2a0b523bda03649124f75924
Brian Miller <brian@theinkshop.ie>
2020-09-12 11:12:06 +0100

    Changed context to include VAT calculation and basket total inc vat

--- basket/contexts.py
+++ basket/contexts.py
@@ -11,12 +11,16 @@ from products.models import Supplies
 def basket_contents(request):
     basket_items = []
     sub_total = 0
+    total_vat = 0
+    basket_total = 0
 
     basket = request.session.get('basket', {})
 
     for item, quantity in basket.items():
         product = get_object_or_404(Supplies, pk=item)
         sub_total += quantity * product.price
+        total_vat += (product.calculate_inc_vat_price() -
+                      product.price) * quantity
         basket_items.append({
             'id': item,
             'quantity': quantity,
@@ -32,7 +36,7 @@ def basket_contents(request):
 
     # not including delivery charge unless items have been added to basket
     if sub_total > 0:
-        basket_total = sub_total + delivery
+        basket_total = sub_total + total_vat + delivery
     else:
         basket_total = 0
 
@@ -42,6 +46,7 @@ def basket_contents(request):
         'delivery': delivery,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'free_delivery_delta': free_delivery_delta,
+        'total_vat': total_vat,
         'basket_total': basket_total,
     }
 
