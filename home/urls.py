81fffb86d6e1754c96a0567ce1d61050f90e45cc
Brian Miller <brian@theinkshop.ie>
2020-09-18 17:10:28 +0100

    Login and Signup page reworked into 1 page and restyled

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('account/', views.login, name='account'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from allauth.account.forms import LoginForm, SignupForm
 
 
 def index(request):
@@ -6,3 +7,18 @@ def index(request):
     A view to render the index page template
     """
     return render(request, 'home/index.html')
+
+
+def login(request):
+    """
+    A view to render the login page
+    """
+    login_form = LoginForm()
+    signup_form = SignupForm()
+
+    context = {
+        'login_form': login_form,
+        'signup_form': signup_form,
+    }
+
+    return render(request, 'login.html', context)
