216e1c62861996c5cf10ba72b744877dcb0ecc8d
Brian Miller <brian@theinkshop.ie>
2020-09-19 13:40:11 +0100

    Added orders to admin and added create/update/delete signals for orders

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemAdminInline(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ('total_price',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date', 'order_subtotal', 'order_VAT',
+                       'delivery_cost', 'grand_total', 'payment_processor', 'payment_id')
+
+    fields = ('order_number', 'date', 'customer_name', 'email',
+              'phone_number', 'order_address_line1', 'order_town_or_city',
+              'order_county', 'order_country', 'order_post_code',
+              'order_subtotal', 'order_VAT', 'delivery_cost', 'grand_total',
+              'payment_processor', 'payment_id')
+
+    list_display = ('order_number', 'date', 'customer_name',
+                    'order_subtotal', 'order_VAT', 'delivery_cost', 'grand_total',
+                    'payment_processor', 'payment_id')
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
